b50bc8530369ff7a66aea30d6c4b9e83f02fd151
Umarbek Yoqubov <umarbekyoqubov39@gmail.com>
2026-06-22 16:58:51 +0300

    finif

--- src/App.jsx
+++ src/App.jsx
@@ -82,11 +82,11 @@ function APP() {
           </div>
         </section>
 
-        <section className="continer n1">
-          <div className="user-information">
+        <section className=" n1">
+          <div className="user-information continer">
             <div className="user-text">
               <div className="frist-rext">
-                <h4>PHOTOS</h4>
+                <h4 className="ph">PHOTOS</h4>
                 <h1>All Creative Projects</h1>
               </div>
               <div className="seconod-tect">
@@ -100,6 +100,90 @@ function APP() {
               <div>
                 <img src="6.png" alt="" />
               </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="clients continer">
+          <div className="clients-text">
+            <h4 className="ph">clients</h4>
+            <h1>Big DeaWith</h1>
+          </div>
+          <div className="socials">
+            <a href="https://slack.com/">
+              <div className="app">
+                <img src="/slack.svg" alt="" />
+                <h5>Slack</h5>
+              </div>
+            </a>
+            <a href="https://medium.com/">
+              <div className="app">
+                <img src="/medium.svg" alt="" />
+                <h5>Medium</h5>
+              </div>
+            </a>
+            <a href="https://www.microsoft.com/en-us">
+              <div className="app">
+                <img src="/Micrasorf.svg" alt="" />
+                <h5>Microsoft</h5>
+              </div>
+            </a>
+            <a href="https://zeplin.io/">
+              <div className="app">
+                <img src="/Zeplin.svg" alt="" />
+                <h5>Zeplin</h5>
+              </div>
+            </a>
+            <a href="https://www.shopify.com/">
+              <div className="app">
+                <img src="/Shopify.svg" alt="" />
+                <h5>Shopify</h5>
+              </div>
+            </a>
+          </div>
+        </section>
+        {/* next sec */}
+
+        <section className="UI">
+          <div className="continer  ui">
+            <div className="uibox">
+              <h4 className="ph">Testimonials</h4>
+              <h1>What People Says</h1>
+            </div>
+            <div className="user-info-box">
+              <img src="/1.svg" alt="" />
+              <p>
+                Quote testimonials demonstrate support for your product or
+                feature <br /> from a user who has experience with it. This can
+                be significantly more <br /> effective than traditional
+                advertising methods as most consumers will trust.
+              </p>
+
+              <img className="g16" src="/16.svg" alt="" />
+              <h3>Michal John</h3>
+            </div>
+          </div>
+        </section>
+
+        <section className="UI">
+          <div className="continer blogs">
+            <div className="blog-text">
+              <p className="ph">Blog</p>
+              <h1>Get News Feeds</h1>
+            </div>
+            <div className="blog">
+              <img src="/13.svg" alt="" />
+              <p className="ph">20 June/2020</p>
+              <h2>
+                Technology is evolving rapidly. <br /> New devices and
+                applications.
+              </h2>
+            </div>
+            <div className="blog">
+              <img className="blog-img" src="/13.svg" alt="" />
+            </div>
+            <div className="blog">
+              <img className="blog-img" src="/13.svg" alt="" />
             </div>
           </div>
         </section>
